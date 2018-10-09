ac7d78c71e3d56520b0c1fd026ca448527baa33d
Apoorv Agarwal <aapoorv41@gmail.com>
2018-10-09 18:29:06 +0530

    fixing 503 error on heroku

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ app.use(bodyParser.json())
 app.use('/api', require('./routes/apimedicapi'))
 
 // just for developement purposes
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/dist'))
-  const path = require('path')
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-  })
-}
+// if (!process.env.NODE_ENV === 'production') {
+app.use(express.static('client/dist'))
+const path = require('path')
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+})
+// }
 
 app.listen(config.PORT, function() {
   console.log('Server started')
